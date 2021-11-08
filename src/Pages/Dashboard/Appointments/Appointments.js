9116ebb4b53a9e2d96e24e30f6764c640cb21d78
Rony163 <minhajulislamrony43@gmail.com>
2021-11-08 17:03:20 +0600

    all check

--- src/Pages/Dashboard/Appointments/Appointments.js
+++ src/Pages/Dashboard/Appointments/Appointments.js
@@ -13,7 +13,7 @@ const Appointments = ({ date }) => {
     const [appointments, setAppointments] = useState([])
 
     useEffect(() => {
-        const url = `https://stark-caverns-04377.herokuapp.com/appointments?email=${user.email}&date=${date.toLocaleDateString()}`
+        const url = `http://localhost:5000/appointments?email=${user.email}&date=${date.toLocaleDateString()}`
         fetch(url, {
             headers: {
                 'authorization': `Bearer ${token}`
